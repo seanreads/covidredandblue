88bc46cdb758c834f7d01a1ee48a310e5c61d137
Sean O'Donnell <seano@Seans-MacBook-Pro.local>
2020-10-07 14:38:34 -0400

    cleanup functions into helper.js

---
+++ helper.js
@@ -0,0 +1,44 @@
+const formatNumber = (number,mask) => numeral(number).format(mask);
+const formatPercentage = (number,mask) => numeral(number).format(mask);
+
+const rowClassForPartyControl = (partyControl) => { 
+    let className;
+    if (partyControl == 'R')
+        className = 'rowRepublicanControl';
+    else if (partyControl == 'D')
+        className = 'rowDemocratControl';
+    else if (partyControl == 'S')
+        className = 'rowSplitControl'
+    else
+        className = 'unknownControl'
+    return className;
+}
+
+const getNewCasesNextWeeksKey = nextNumberOfWeeks => `NewCases_next${nextNumberOfWeeks}wk`;
+const getNewDeathsNextWeeksKey = nextNumberOfWeeks => `NewDeaths_next${nextNumberOfWeeks}wk`;
+
+const getNewCasesKey = (showPer100k, show7DayAvg) => {
+    let newCasesKey = '';
+    if (showPer100k && show7DayAvg)
+        newCasesKey = 'NewCases_7da_100k';
+    else if (showPer100k)
+        newCasesKey = 'NewCases_100k';
+    else if (show7DayAvg)
+        newCasesKey = 'NewCases_7da';
+    else
+        newCasesKey = 'NewCases';
+    return newCasesKey;
+}
+
+const getNewDeathsKey = (showPer100k, show7DayAvg) => {
+    let newDeathsKey = '';
+    if (showPer100k && show7DayAvg)
+        newDeathsKey = 'NewDeaths_7da_100k';
+    else if (showPer100k)
+        newDeathsKey = 'NewDeaths_100k';
+    else if (show7DayAvg)
+        newDeathsKey = 'NewDeaths_7da';
+    else
+        newDeathsKey = 'NewDeaths';
+    return newDeathsKey;
+}
